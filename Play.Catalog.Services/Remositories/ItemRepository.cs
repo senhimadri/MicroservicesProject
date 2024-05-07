22193efd6f57fb998a01de1804b5010442f3fcfa
senhimadri <senhimadri.dev@gmail.com>
2024-05-07 12:16:42 +0600

    Mongo DB Contex Config

---
+++ Play.Catalog.Services/Remositories/ItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+using Play.Catalog.Services.Entities;
+
+namespace Play.Catalog.Services.Remositories;
+
+public class ItemRepository
+{
+    private const string collectionName = "items";
+    private readonly IMongoCollection<Item> dbCollection;
+
+    private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+}
+
