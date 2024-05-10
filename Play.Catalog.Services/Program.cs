2feb14d3169d4f14df034a2fdc4b7b3e3a6865e1
senhimadri <senhimadri.dev@gmail.com>
2024-05-10 13:48:13 +0600

    Creating Generic Repos

--- Play.Catalog.Services/Program.cs
+++ Play.Catalog.Services/Program.cs
@@ -1,10 +1,6 @@
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
-using MongoDB.Driver;
-using Play.Catalog.Services.Entities;
-using Play.Catalog.Services.Remositories;
 using Play.Catalog.Services.Settings;
+using Play.Catalog.Services.Remositories;
+using Play.Catalog.Services.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,22 +11,8 @@ builder.Services.AddControllers(options=>
 
 var servicesSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
-builder.Services.AddSingleton(serviceProvider =>
-{
-    var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    var MongoClient = new MongoClient( mongoDbSettings!.ConnectionString ?? string.Empty );
-    return MongoClient.GetDatabase(servicesSettings!.ServiceName ?? string.Empty );
-});
 
-builder.Services.AddSingleton<IRepository<Item>>(serviceProvider=>
-{
-    var database = serviceProvider.GetService<IMongoDatabase>();
-    return new MongoRepository<Item>(database!, "items");
-});
-
-BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
+builder.Services.AddMongo().AddMongoRepository<Item>("items");
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
