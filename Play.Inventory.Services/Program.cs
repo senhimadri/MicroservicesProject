abe9c4b0ab99b612848d3a948e3efbabe0365476
senhimadri <senhimadri.dev@gmail.com>
2024-05-16 08:33:00 +0600

    Exceptions Handled

--- Play.Inventory.Services/Program.cs
+++ Play.Inventory.Services/Program.cs
@@ -1,17 +1,33 @@
+using Microsoft.Extensions.DependencyInjection;
 using Play.Common.MongoDb;
 using Play.Inventory.Services.Clients;
 using Play.Inventory.Services.Entities;
+using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 
 builder.Services.AddMongo()
                 .AddMongoRepository<InventoryItems>("inventoryitems");
 
+
+
 builder.Services.AddHttpClient<CatalogClient>(client=>
 {
     client.BaseAddress = new Uri("https://localhost:7138");
-});
+})
+    .AddTransientHttpErrorPolicy(builders => builders.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+        retryCount: 5,
+        sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2,retryAttempt)),
+        onRetry:(outcome, timeSpan, retryAttempt)=>
+        {
+            var serviceProvider = builder.Services.BuildServiceProvider();
+            serviceProvider.GetService<ILogger<CatalogClient>>()?
+                .LogWarning($"Retry attempt {retryAttempt} due to {outcome.Exception}.");
+        }))
+    .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(1)));
 
 builder.Services.AddControllers();
 
