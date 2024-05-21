fddf66238eb85d85c58503cc536e49932a75a717
senhimadri <senhimadri.dev@gmail.com>
2024-05-21 16:21:54 +0600

    Few Changes

--- Play.Catalog.Contracts/Contracts.cs
+++ Play.Catalog.Contracts/Contracts.cs
@@ -2,4 +2,4 @@
 
 public record CatalogItemCreated(Guid ItemId,string Name, string Description);
 public record CatalogItemUpdated(Guid ItemId, string Name, string Description);
-public record CatalogItemDeleted (Guid ItemId);
+public record CatalogItemDeleted(Guid ItemId);
