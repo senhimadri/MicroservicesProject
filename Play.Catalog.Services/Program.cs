74edfe75c7d7e04c399a295a716217e6e2e4f7e8
senhimadri <senhimadri.dev@gmail.com>
2024-05-21 17:02:43 +0600

    Setup Catalog Project for MassTransit

--- Play.Catalog.Services/Program.cs
+++ Play.Catalog.Services/Program.cs
@@ -1,4 +1,6 @@
+using MassTransit;
 using Play.Catalog.Services.Entities;
+using Play.Catalog.Services.Settings;
 using Play.Common.MongoDb;
 using Play.Common.Settings;
 
@@ -13,6 +15,20 @@ var servicesSettings = builder.Configuration.GetSection(nameof(ServiceSettings))
 
 
 builder.Services.AddMongo().AddMongoRepository<Item>("items");
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context,configurator)=>
+    {
+        var rabbitMQSettings = builder.Configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+        configurator.Host(rabbitMQSettings!.Host);
+        configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(prefix: servicesSettings!.ServiceName, includeNamespace: false));
+    });
+});
+
+
+//builder.Services.AddMassTransitHostedService();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ Play.Catalog.Services/Settings/RabbitMQSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Play.Catalog.Services.Settings;
+
+public class RabbitMQSettings
+{
+    public string Host { get; init; } = string.Empty;
+
+}
+
